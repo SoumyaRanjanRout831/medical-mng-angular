33e16a34e5d6eee38b687f234ceec5e87e38703b
soumyaranjanrout831 <soumyaranjanrout831@gmail.com>
2025-03-21 10:32:58 +0530

    component created

---
+++ src/app/medicine/add-medicine/add-medicine.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-medicine',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-medicine.component.html',
+  styleUrl: './add-medicine.component.scss'
+})
+export class AddMedicineComponent {
+
+}
